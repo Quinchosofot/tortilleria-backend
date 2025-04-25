f7376c5b33420c3ea7d26ef4dcbed33d22ea10b7
CARLOS MARROQUIN CALLEJAS <carlos.marroquincallejas@conduent.com>
2025-04-25 11:13:14 -0600

    conectando al backend de pedidos

--- frontend/upload git.bat
+++ frontend/upload git.bat
@@ -1,7 +1,8 @@
 @echo off
 echo ====== Subiendo cambios a GitHub ======
+set /p COMENTARIO=Escribe el mensaje de commit: 
 git add .
-git commit -m "Soporte para Alexa HTTPS"
+git commit -m "%COMENTARIO%"
 git push
 echo ====== Listo. Cambios subidos a GitHub. ======
 pause
